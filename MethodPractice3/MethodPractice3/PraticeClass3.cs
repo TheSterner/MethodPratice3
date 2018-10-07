6f3bd6725870919798f06bd7dbb58f2fdf6e4080
William B McAllister <blakemcallister77@gmail.com>
2018-10-07 18:22:35 -0400

    x

---
+++ MethodPractice3/MethodPractice3/PraticeClass3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodPractice3
+{
+	public class PraticeClass3
+	{
+		public static int AddedUserNum(int j)
+		{
+			return j + 10;
+		}
+
+		public static int AddedUserNum(int j, int k = 0)
+		{
+			return (j + 10) + k;
+		}
+	}
+}
---
+++ MethodPractice3/MethodPractice3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodPractice3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+			Console.WriteLine("Please enter a number between 1-50");
+			int userNum = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Please enter another number between 1-50 or don't.  You don't have to if you don't want to.");
+			int userNum2 = Convert.ToInt32(Console.ReadLine());
+			
+
+			
+			int userNum3 = PraticeClass3.AddedUserNum(userNum, userNum2);
+			Console.WriteLine("Here is the output of this method : Your first number is now : " + userNum3);
+			Console.ReadLine();
+
+		}
+	}
+}
