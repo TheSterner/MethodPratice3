e4db1f5ccdf8d46696b92e5bc2ffd7eb6c61f0b7
William B McAllister <blakemcallister77@gmail.com>
2018-10-07 21:32:45 -0400

    3

--- MethodPractice3/MethodPractice3/Program.cs
+++ MethodPractice3/MethodPractice3/Program.cs
@@ -10,18 +10,28 @@ namespace MethodPractice3
 	{
 		static void Main(string[] args)
 		{
-			
-			Console.WriteLine("Please enter a number between 1-50");
-			int userNum = Convert.ToInt32(Console.ReadLine());
-			Console.WriteLine("Please enter another number between 1-50 or don't.  You don't have to if you don't want to.");
-			int userNum2 = Convert.ToInt32(Console.ReadLine());
-			
+			var empty = false;
 
-			
-			int userNum3 = PraticeClass3.AddedUserNum(userNum, userNum2);
-			Console.WriteLine("Here is the output of this method : Your first number is now : " + userNum3);
-			Console.ReadLine();
+			while (!empty)
+			{
+				Console.WriteLine("Please enter a number between 1-50");
+				int userNum = Convert.ToInt32(Console.ReadLine());
+				Console.WriteLine("Please enter another number between 1-50 or don't.  You don't have to if you don't want to.");
+				var userNum2 = Console.ReadLine();
+				empty = !string.IsNullOrWhiteSpace(userNum2) && userNum2.All(c => c >= '0' && c <= '9');
+				int userNum3 = Convert.ToInt32(userNum2);
+				//int userNum4 = PraticeClass3.AddedUserNum(userNum);
+				//int userNum5 = PraticeClass3.AddedUserNum(userNum, userNum3);
+				Console.WriteLine("Here is the output of this method : " + PraticeClass3.AddedUserNum(userNum, userNum3));
 
+				if (!empty)
+					Console.WriteLine(PraticeClass3.AddedUserNum(userNum));
+			}
+			 
+
+				
+				Console.ReadLine();
+			
 		}
 	}
 }
